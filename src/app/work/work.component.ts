3ea8891fd8fae3a682c5aaed8c48ccdfd93281c6
Lautaro Colella <lautarocolella1@gmail.com>
2023-03-07 03:36:42 -0600

    Done order by in work page

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -12,14 +12,15 @@ export class WorkComponent {
   
 
   array_all:            Array<{ page: any }> = [];
-  array_page:           Array<{ id: number, name: string, code_uri: string, live_uri: string, image_uri: string, image_alt: string, date: Date, description: string }> = [];
   pageIndex:            number = 0;
   total_pages:          number = 1;
   show_arrows:          boolean = false;
   project_items:        Array<{ id: number, name: string, code_uri: string, live_uri: string, image_uri: string, image_alt: string, date: Date, description: string }> = [];
   page_selector_items:  Array<{ number: number }> = [{ number: 1 }];
+  current_order:        string = "AZ";
 
-  onRightArrow(){
+ 
+ onRightArrow(): void {
     this.pageIndex++;
     if(this.pageIndex > this.total_pages - 1){
       this.pageIndex = 0;
@@ -34,7 +35,7 @@ export class WorkComponent {
       }
     }
   }
-  onLeftArrow(){
+  onLeftArrow(): void {
     this.pageIndex--;
     if(this.pageIndex < 0){
       this.pageIndex = this.total_pages - 1;
@@ -50,9 +51,91 @@ export class WorkComponent {
     }
   }
 
-  changePage(page_number: number){
+  changePage(page_number: number): void {
     this.pageIndex = page_number - 1;
   }
+
+  orderBy(order: string): void {
+    if(this.current_order !== order){
+      const possible_order: string[] = ["NL", "LN", "AZ", "ZA"];
+      
+      if(order === possible_order[0]){
+        this.project_items.sort(function (a, b){
+          return +new Date(b.date) - +new Date(a.date);
+        });
+      }
+      else if(order === possible_order[1]){
+        this.project_items.sort(function (a, b){
+          return +new Date(a.date) - +new Date(b.date);
+        });
+      }
+      else if(order === possible_order[2]){
+        this.project_items.sort(function (a, b){
+          return a.name.localeCompare(b.name);
+        });
+      }
+      else if(order === possible_order[3]){
+        this.project_items.sort(function (a, b){
+          return b.name.localeCompare(a.name);
+        })
+      }
+      this.current_order = order;
+      this.createPages();
+    }
+  }
+
+  createPages(): void {
+    let array_page: Array<{ id: number, name: string, code_uri: string, live_uri: string, image_uri: string, image_alt: string, date: Date, description: string }> = [];
+    let start_at: number      = 0;
+    this.array_all            = [];
+    this.total_pages          = 1;
+    this.pageIndex            = 0;
+    this.page_selector_items  = [{ number: 1 }]
+
+    for(let i: number = start_at; i < this.project_items.length; i++){
+      if(i%2 === 0 && i%3 === 0 && i !== start_at){
+        if(!this.show_arrows){
+          this.show_arrows = true;
+        }
+
+        this.page_selector_items.push({
+          number: this.total_pages + 1
+        });          
+
+        this.total_pages++;
+        this.array_all.push({page: array_page});
+        array_page = [];
+        array_page.push({
+          id: this.project_items[i].id,
+          name: this.project_items[i].name,
+          code_uri: this.project_items[i].code_uri,
+          live_uri: this.project_items[i].live_uri,
+          image_uri: this.project_items[i].image_uri,
+          image_alt: this.project_items[i].image_alt,
+          date: this.project_items[i].date,
+          description: this.project_items[i].description
+        });
+        start_at = i;
+      }
+      else{
+        array_page.push({
+          id: this.project_items[i].id,
+          name: this.project_items[i].name,
+          code_uri: this.project_items[i].code_uri,
+          live_uri: this.project_items[i].live_uri,
+          image_uri: this.project_items[i].image_uri,
+          image_alt: this.project_items[i].image_alt,
+          date: this.project_items[i].date,
+          description: this.project_items[i].description
+        });
+        if(i === this.project_items.length - 1){
+          this.array_all.push({page: array_page});
+          array_page = [];
+        }
+      }
+    }
+  }
+
 
   ngOnInit(){
 
@@ -64,49 +147,7 @@ export class WorkComponent {
         }
       }
 
-      let start_at: number = 0;
-      for(let i: number = start_at; i < this.project_items.length; i++){
-        if(i%2 === 0 && i%3 === 0 && i !== start_at){
-          if(!this.show_arrows){
-            this.show_arrows = true;
-          }
-
-          this.page_selector_items.push({
-            number: this.total_pages + 1
-          });          
-
-          this.total_pages++;
-          this.array_all.push({page: this.array_page});
-          this.array_page = [];
-          this.array_page.push({
-            id: this.project_items[i].id,
-            name: this.project_items[i].name,
-            code_uri: this.project_items[i].code_uri,
-            live_uri: this.project_items[i].live_uri,
-            image_uri: this.project_items[i].image_uri,
-            image_alt: this.project_items[i].image_alt,
-            date: this.project_items[i].date,
-            description: this.project_items[i].description
-          });
-          start_at = i;
-        }
-        else{
-          this.array_page.push({
-            id: this.project_items[i].id,
-            name: this.project_items[i].name,
-            code_uri: this.project_items[i].code_uri,
-            live_uri: this.project_items[i].live_uri,
-            image_uri: this.project_items[i].image_uri,
-            image_alt: this.project_items[i].image_alt,
-            date: this.project_items[i].date,
-            description: this.project_items[i].description
-          });
-          if(i === this.project_items.length - 1){
-            this.array_all.push({page: this.array_page});
-            this.array_page = [];
-          }
-        }
-      }
+      this.orderBy("NL");
     });
   }
 }
