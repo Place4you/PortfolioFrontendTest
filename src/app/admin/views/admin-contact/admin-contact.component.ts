de8fbacee5577111da4d8134a2e8e3b11205bda6
Lautaro Colella <lautarocolella1@gmail.com>
2023-04-14 16:51:03 -0500

    Admin page: contact view crud - Contact page: show item id, change hover color

--- src/app/admin/views/admin-contact/admin-contact.component.ts
+++ src/app/admin/views/admin-contact/admin-contact.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'
+import { CookieService } from 'ngx-cookie-service';
+import { ContactService } from '../../../rest/contact.service';
 
 @Component({
   selector: 'app-admin-contact',
@@ -6,5 +9,202 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-contact.component.scss']
 })
 export class AdminContactComponent {
+
+  constructor(private router: Router, private cookieService: CookieService, private contactService: ContactService) { }
+
+  error_message_add: string = "";
+  error_message_edit: string = "";
+  error_message_delete: string = "";
+  current_value: string = "add";
+  found_item_id: boolean = false;
+  item_to_edit: any = {};
+
+  reset_default_values(): void {
+    this.error_message_add    = "";
+    this.error_message_edit   = "";
+    this.error_message_delete = "";
+    this.current_value        = "add";
+    this.found_item_id        = false;
+  }
+
+  method_change(event: any): void {
+    const value = event.target.value;
+    if(value !== this.current_value){
+      if(value === "add"){
+        this.reset_default_values();
+        this.current_value = "add";
+      }
+      else if(value === "edit"){
+        this.reset_default_values();
+        this.current_value = "edit";
+      }
+      else if(value === "delete"){
+        this.reset_default_values();
+        this.current_value = "delete";
+      }
+    }
+  }
+
+
+  create_item(
+    name: string,
+    account: string,
+    uri: string,
+    image_uri: string,
+    image_alt: string
+  ): void {
+
+    if(!name){
+      this.error_message_add = "Field <name> can't be null";
+    }
+    else if(!account){
+      this.error_message_add = "Field <account> can't be null";
+    }
+
+    else {
+      if(!this.cookieService.get('JWT')){
+        this.router.navigate(['login']);
+      }
+      else{
+        const cookieValue: string = this.cookieService.get('JWT');
+        this.contactService.createItem(cookieValue, name, account, uri, image_uri, image_alt)
+        .subscribe((response: any) => {
+          if(response === "0"){
+            this.router.navigate(['login']);
+          }
+          else {
+            this.router.navigate(['home']);
+            this.error_message_add = "";
+          }
+        });
+      }
+    }
+  }
+
+  search_edit_item(inputId: string): void {
+    const itemId = Number(inputId) || 0;
+    if(itemId === 0){
+      this.error_message_edit = "Invalid item id";
+    }
+    else if(itemId < 1){
+      this.error_message_edit = "The item id must be greater than 0";
+    }
+    else if(itemId > 65535){
+      this.error_message_edit = "The item id must be lesser than 65536";
+    }
+    else {
+      this.contactService.getItem(itemId)
+      .subscribe((response: any) => {
+        if(response === "0"){
+          this.router.navigate(['login']);
+        }
+        else {
+          this.error_message_edit = "";
+          this.found_item_id = true;
+          this.item_to_edit = {
+            id: response.body.id,
+            name: response.body.name,
+            account: response.body.account,
+            uri: response.body.link,
+            image_uri: response.body.image_uri,
+            image_alt: response.body.image_alt
+          }
+        }
+      });
+    }
+  }
+
+  edit_item(
+    name: string,
+    account: string,
+    uri: string,
+    image_uri: string,
+    image_alt: string
+  ): void {
+    if(!name){
+      this.error_message_edit = "Field <name> can't be null";
+    }
+    else if(!account){
+      this.error_message_edit = "Field <account> can't be null";
+    }
+    else {
+      if(this.item_to_edit.name){
+        let something_changed: boolean = false;
+        if(name !== this.item_to_edit.name){
+          something_changed = true;
+        }
+        if(account !== this.item_to_edit.account){
+          something_changed = true;
+        }
+        if(uri !== this.item_to_edit.uri){
+          something_changed = true;
+        }
+        if(image_uri !== this.item_to_edit.image_uri){
+          something_changed = true;
+        }
+        if(image_alt !== this.item_to_edit.image_alt){
+          something_changed = true;
+        }
+
+        if(something_changed){
+          if(!this.cookieService.get('JWT')){
+            this.router.navigate(['login']);
+          }
+          else{
+            const cookieValue: string = this.cookieService.get('JWT');
+            this.contactService.updateItem(cookieValue, this.item_to_edit.id, name, account, uri, image_uri, image_alt)
+            .subscribe((response: any) => {
+              if(response === "0"){
+                this.router.navigate(['login']);
+              }
+              else {
+                this.router.navigate(['home']);
+                this.error_message_edit = "";
+              }
+            });
+          }
+        }
+        else {
+          this.error_message_edit = "Item not edited";
+        }
+      }
+      else {
+        this.error_message_edit = "Item to update not found";
+        this.found_item_id = false;
+      }
+    }
+  }
+
+  delete_item(inputId: string): void {
+    const itemId = Number(inputId) || 0;
+    if(itemId === 0){
+      this.error_message_delete = "Invalid item id";
+    }
+    else if(itemId < 1){
+      this.error_message_delete = "The item id must be greater than 0";
+    }
+    else if(itemId > 65535){
+      this.error_message_delete = "The item id must be lesser than 65536";
+    }
+    else {
+      if(!this.cookieService.get('JWT')){
+        this.router.navigate(['login']);
+      }
+      else{
+        const cookieValue: string = this.cookieService.get('JWT');
+        this.contactService.deleteItem(cookieValue, itemId)
+        .subscribe((response: any) => {
+          if(response === "0"){
+            this.router.navigate(['login']);
+          }
+          else {
+            this.router.navigate(['home']);
+            this.error_message_delete = "";
+          }
+        });
+      }
+    }
+  }
+
 
 }
--- src/app/rest/contact.service.ts
+++ src/app/rest/contact.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http'
+import { HttpClientModule, HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http'
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,107 @@ export class ContactService {
 
   constructor(private http: HttpClient) { }
 
-  getItems(){
-    return this.http.get("http://localhost:8080/api/v1/contact_item", {observe: 'response', responseType: 'json'});
+  uri: string = "http://localhost:8080/api/v1/contact_item"
+
+  getItem(id: number){
+    return this.http.get(
+      this.uri + `/${id}`,
+      {
+        observe: 'response',
+        responseType: 'json'
+      })
+    .pipe(
+      catchError((err: HttpErrorResponse) => {
+        return "0";
+      })
+    );
   }
+
+  getItems(){
+    return this.http.get(
+      this.uri,
+      {
+        observe: 'response',
+        responseType: 'json'
+      });
+  }
+
+  createItem(
+    token: string,
+    name: string,
+    account: string,
+    link: string,
+    image_uri: string,
+    image_alt: string
+    ){
+    const body = {
+      name: name,
+      account: account,
+      link: link,
+      image_uri: image_uri,
+      image_alt: image_alt
+    }
+    return this.http.post(
+      this.uri + "/add",
+      body,
+      {
+        headers: {'Authorization':token},
+        observe: 'response',
+        responseType: 'json'
+      })
+    .pipe(
+      catchError((err: HttpErrorResponse) => {
+        return "0";
+      })
+    );
+  }
+
+  updateItem(
+    token: string,
+    id: number,
+    name: string,
+    account: string,
+    link: string,
+    image_uri: string,
+    image_alt: string
+  ){
+    const body = {
+      id: id,
+      name: name,
+      account: account,
+      link: link,
+      image_uri: image_uri,
+      image_alt: image_alt
+    }
+    return this.http.put(
+      this.uri + "/update",
+      body,
+      {
+        headers: {'Authorization':token},
+        observe: 'response',
+        responseType: 'json'
+      })
+    .pipe(
+      catchError((err: HttpErrorResponse) => {
+        return "0";
+      })
+    );
+  }
+
+
+  deleteItem(token: string, id: number){
+    return this.http.delete(
+      this.uri + `/delete/${id}`,
+      {
+        headers: {'Authorization':token},
+        observe: 'response',
+        responseType: 'json'
+      })
+    .pipe(
+      catchError((err: HttpErrorResponse) => {
+        return "0";
+      })
+    );
+  }
+
 }
