d96c464dd86b9303a114afc6200ef963f4c14484
Lautaro Colella <lautarocolella1@gmail.com>
2023-05-06 13:35:45 -0500

    Responsive navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,12 +12,30 @@ import { CookieService } from 'ngx-cookie-service';
 export class NavbarComponent {
 
 	currentRoute: string | undefined = undefined;
+	nav_open: boolean = false;
 
 	constructor (private router: Router, private cookieService: CookieService) {
 		this.currentRoute = router.url;
 	}
 
-	isLogged(){
+	toggle_nav(): void {
+		const all_buttons: HTMLElement | null = document.getElementById('navigationbar');
+		const nav_toggle: HTMLElement | null = document.getElementById('toggle_nav');
+		if(all_buttons !== null && nav_toggle !== null){
+			if(this.nav_open){
+				this.nav_open = false;
+				all_buttons.style.display = 'none';
+				nav_toggle.style.left = '0';
+			}
+			else {
+				this.nav_open = true;
+				all_buttons.style.display = 'block';
+				nav_toggle.style.left = '180px';
+			}
+		}
+	}
+
+	isLogged(): boolean {
 		return this.cookieService.get('JWT') ? true : false;
 	}
 }
