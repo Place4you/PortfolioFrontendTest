dc7f8581095fb6bf0d360d00506f4e940fe1d4db
Lautaro Colella <lautarocolella1@gmail.com>
2023-05-19 09:58:15 -0500

    add crontask to mantain the server active

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CronTaskService } from './shared/services/crontask_for_backend.service';
 
 @Component({
 	selector: 'app-root',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title: string = 'portfolio';
+
+	constructor(private cronTaskService: CronTaskService) {}
+
+	ngOnInit(): void {
+		this.cronTaskService.startPeriodicExecution();
+	}
 }
