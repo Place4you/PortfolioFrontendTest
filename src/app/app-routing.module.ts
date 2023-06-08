b29417ddb7a57e38cb901440db324188a304dfe7
Lautaro Colella <lautarocolella1@gmail.com>
2023-06-08 08:05:54 -0500

    Lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from '@@pages/index/index.component';
-import { WorkComponent } from '@@pages/work/work.component';
-import { AboutComponent } from '@@pages/about/about.component';
-import { ContactComponent } from '@@pages/contact/contact.component';
-import { LoginComponent } from '@@pages/login/login.component';
-import { AdminComponent } from '@@pages/admin/admin.component';
 import { Page404Component } from '@@pages/errors/page404/page404.component';
 import { Page403Component } from '@@pages/errors/page403/page403.component';
 import { Page500Component } from '@@pages/errors/page500/page500.component';
 
 const routes: Routes = [
-	{path: '', redirectTo: '/home', pathMatch: 'full'},
-	{path: 'home', component:IndexComponent},
-	{path: 'work', component:WorkComponent},
-	{path: 'about', component:AboutComponent},
-	{path: 'contact', component:ContactComponent},
-	{path: 'login', component:LoginComponent},
-	{path: 'admin', component:AdminComponent},
-	{path: 'error403', component:Page403Component},
-	{path: 'error500', component:Page500Component},
-	{path: '**', component:Page404Component}
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
+	{ path: 'home', component: IndexComponent },
+	{ path: 'work', loadChildren: () => import('@@pages/work/work.module').then(m => m.WorkModule) },
+	{ path: 'about', loadChildren: () => import('@@pages/about/about.module').then(m => m.AboutModule) },
+	{ path: 'contact', loadChildren: () => import('@@pages/contact/contact.module').then(m => m.ContactModule) },
+	{ path: 'login', loadChildren: () => import('@@pages/login/login.module').then(m => m.LoginModule) },
+	{ path: 'admin', loadChildren: () => import('@@pages/admin/admin.module').then(m => m.AdminModule) },
+	{ path: 'error403', component: Page403Component },
+	{ path: 'error500', component: Page500Component },
+	{ path: '**', component: Page404Component }
 ];
 
 @NgModule({
--- src/app/pages/work/work.module.ts
+++ src/app/pages/work/work.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { WorkComponent } from './work.component';
 
 @NgModule({
-	declarations: [],
-	imports: [ CommonModule ]
+	declarations: [ ],
+	imports: [
+		CommonModule,
+		RouterModule.forChild([{ path: '', component: WorkComponent }])
+	]
 })
 export class WorkModule { }
